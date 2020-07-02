f872045d062b983ae87e57a82d043c2c5fb9a9f4
amal <amal.gregri@icloud.com>
2020-07-02 09:50:10 +0300

    add README.md + modified main.js

---
+++ README.md
@@ -0,0 +1,16 @@
+# Calculator
+[![GitHub issues](https://img.shields.io/github/issues/Amal337/calculator)](https://github.com/Amal337/calculator/issues)  [![GitHub forks](https://img.shields.io/github/forks/Amal337/calculator)](https://github.com/Amal337/calculator/network)  [![GitHub stars](https://img.shields.io/github/stars/Amal337/calculator)](https://github.com/Amal337/calculator/stargazers)
+
+I created this simple and basic calculator as a part of The Odin Project web development course.
+
+### Technologies:
+- HTML
+- CSS
+- JavaScript
+- VSCode
+
+![screenshot](screenshot.png)
+
+
+https://www.theodinproject.com/
+
--- main.js
+++ main.js
@@ -1,95 +1,116 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
 
-// click sound event to all buttons
-// const sound = document.getElementById('AC');
+  //to display numbers in the screen 
+  function inputDigit(digit) {
+      const { displayValue, waitingForSecondOperand } = calculator;
+      
+      if(waitingForSecondOperand === true){
+          calculator.displayValue = digit;
+          calculator.waitingForSecondOperand = false;
+      } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+      }
+    }
+
+  //Input a decimal point
+function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) return;
+    //if 'dispalyValue' does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)){
+        //append decimal point
+        calculator.displayValue += dot;
+    }
+}
+
+//handling operators
+
+function handelOperator(nextOperator) {
+    const { firstOperand, displayValue, operator} = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand){
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand == null) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const currentValue = firstOperand || 0;
+        const result = performCalculation[operator](currentValue, inputValue);
+        
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '=': (firstOperand, secondOperand) => secondOperand
+};
+
+//Reset the calculator
+function resetCalculator() {
+    calculator.displayValue = '0'
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+//display screen//
+function updateScreen() {
+    const display = document.querySelector(".screen");
+    display.value = calculator.displayValue;
+}
+updateScreen();
+
+
+//operations
+const keys = document.querySelector('.keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event;  //it means const target = event.target
+    if (!target.matches('button')) {
+        //exit the function if it did not click a button
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        handelOperator(target.value);
+        updateScreen();
+        return;
+    }
+    if (target.classList.contains('clear')) {
+        resetCalculator();
+        updateScreen();
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateScreen();
+        return;
+    }
+        
+    inputDigit(target.value);
+    updateScreen();
+
+})
+
+
+
+//     // click sound event to all buttons
+// const sound = document.querySelectorAll('button')
 //       sound.addEventListener('click', playAudio);
-
-// const sound1 = document.getElementById('operators');
-//       sound1.addEventListener('click', playAudio);
-
-// const sound2 = document.getElementById('numbers');
-//       sound2.addEventListener('click', playAudio);
 
 // function playAudio(){
 //     const tick = new Audio('sounds/click.mp3');
 //     tick.play();
 // }
-
-//numbers buttons
-// const btn = document.querySelectorAll('.number')
-const btn1 = document.getElementById('1');
-const btn2 = document.getElementById('2');
-const btn3 = document.getElementById('3');
-const btn4 = document.getElementById('4');
-const btn5 = document.getElementById('5');
-const btn6 = document.getElementById('6');
-const btn7 = document.getElementById('7');
-const btn8 = document.getElementById('8');
-const btn9 = document.getElementById('9');
-const btn0 = document.getElementById('0');
-
-
-btn1.onclick = function() {num1.innerHTML+= Number('1');}
-btn2.onclick = function() {num1.innerHTML+= Number('2');}
-btn3.onclick = function() {num1.innerHTML+= Number('3');}
-btn4.onclick = function() {num1.innerHTML+= Number('4');}
-btn5.onclick = function() {num1.innerHTML+= Number('5');}
-btn6.onclick = function() {num1.innerHTML+= Number('6');}
-btn7.onclick = function() {num1.innerHTML+= Number('7');}
-btn8.onclick = function() {num1.innerHTML+= Number('8');}
-btn9.onclick = function() {num1.innerHTML+= Number('9');}
-btn0.onclick = function() {num1.innerHTML+= Number('0');}
-
-//clear
-const clear = document.getElementById('AC');
-        clear.onclick = function() {num1.innerHTML="";}
-
-//operations
-const plus = document.getElementById('plus');
-const minus = document.getElementById('min');
-const multi = document.getElementById('multi');
-const divide = document.getElementById('divide');
-const equal = document.getElementById('equal');
-// plus.onclick = function() {inputs.innerText+= "+";}
-// minus.onclick = function() {inputs.innerText+= "-";}
-// multi.onclick = function() {inputs.innerText+= "*";}
-// div.onclick = function() {inputs.innerText+= "/";}
-// equal.onclick = function() {inputs.innerText= 'result';}
-
-// function operate(num1, op, num2){
-//    if( op == '+'){
-//       add();
-//    } else if( op == '*'){
-//       multiply();
-//    } else if( op == '/'){
-//       divid();
-//    } else if( op == '-'){
-//       substract();
-//    }
-// }
-
-let inputs = Number(document.getElementById('num1').value);
-// let num2 = Number(document.getElementById('num2').value);
-
-function add(){
-   document.getElementById('equal').innerHTML= num1+num2;
-   return false; 
-}
-   document.getElementById('plus').addEventListener('click', add);
-  
-function subtract(){
-    document.getElementById('equal').innerHTML= num1-num2;
-    return false;
-}
-document.getElementById('min').addEventListener('click', min);
-
-function multiply(){
-    document.getElementById('equal').innerHTML= num1*num2;
-    return false;
-}
-document.getElementById('multi').addEventListener('click', multiply);
-
-function divid(){
-    document.getElementById('equal').innerHTML= num1/num2;
-    return false;
-}
-    document.getElementById('divide').addEventListener('click', divid);
